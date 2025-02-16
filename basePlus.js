d7003760e45443de519db3c8a50fd0b6a874be7f
m9uts <110842525+m9uts@users.noreply.github.com>
2025-02-16 17:50:00 +0500

    Update basePlus.js

--- basePlus.js
+++ basePlus.js
@@ -4,7 +4,43 @@ G.AddData({
 	desc:'',
 	engineVersion:1,
 	manifest:0,
+	requires:['Default dataset*'],
 	func:function(){
+		/*=====================================================================================
+		FIXES
+		=======================================================================================*/
 		
+		/*=====================================================================================
+		RESOURCES
+		=======================================================================================*/
+		
+		new G.Res({
+			name:'heat',
+			displayName:'Heat',
+			desc:'Keeps your tribe warm; each heat reduces illness for 1 people.//Used by some types of crafting.//Will cool over time.',
+			icon:[0,0],
+			replacement:'fire pit',
+			tick:function(me,tick)
+			{
+				if (me.replacement) me.hidden=true; else me.hidden=false;
+				var toSpoil=me.amount*0.001;
+				var spent=G.lose(me.name,randomFloor(toSpoil),'decay');
+			}
+		});
+		
+		/*=====================================================================================
+		RESOURCES
+		=======================================================================================*/
+
+		new G.tech({
+			name:'advancent-fire-sources',
+			desc:'@unlocks [heat]<>[heat] replaces [fire pit]s and does the same thing but only keeps one person warm.',
+			icon:[0,0],
+			cost:{'insight':25},
+			req:{'stone-knapping':true, 'fire-making':true},
+			effects:[
+			],
+			chance:3,
+		});
 	}
 });
